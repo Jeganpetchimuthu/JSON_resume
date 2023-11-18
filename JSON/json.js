4973ecb446d6626f35b7852f566b4a3f9195898b
Arunjegan <127992705+Jeganpetchimuthu@users.noreply.github.com>
2023-11-18 10:17:36 +0530

    Add files via upload

---
+++ JSON/json.js
@@ -0,0 +1,53 @@
+var resume = {
+  name: "jegan",
+  contact: {
+    email: "jegan@gmail.com",
+    phone: "9781465020",
+    address: "arasanoor,sivagangai pin-number-630561",
+  },
+  summary:
+    "Experienced software developer with a passion for creating innovative web applications.",
+  education: [
+    {
+      degree: "Bachelor of Science in Computer Science",
+      institution: "*** clg of engineering",
+      graduation_year: 2018,
+    },
+  ],
+  experience: [
+    {
+      position: "Software Engineer",
+      company: "Tech Solutions Inc.",
+      start_date: "2018-01-01",
+      end_date: "2022-12-31",
+      responsibilities: [
+        "Developed web applications using Python and Django framework.",
+        "Collaborated with cross-functional teams to deliver high-quality software solutions.",
+        "Managed and maintained databases, ensuring data integrity and security.",
+      ],
+    },
+    {
+      position: "Junior Developer",
+      company: "StartUp Co.",
+      start_date: "2017-05-15",
+      end_date: "2017-12-31",
+      responsibilities: [
+        "Assisted in the development and maintenance of web applications.",
+        "Participated in code reviews and debugging sessions.",
+        "Learned and applied modern web development technologies.",
+      ],
+    },
+  ],
+  skills: [
+    "JavaScript",
+    "HTML/CSS",
+    "react",
+    "nodejs",
+    "mongodb",
+    "Version Control (Git)",
+
+    "Problem-Solving",
+    "Teamwork",
+  ],
+};
+console.log(resume);
